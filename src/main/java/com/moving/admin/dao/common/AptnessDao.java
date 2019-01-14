d5b9c03cf7b718cbf4fd6f128e6ac550a0196e60
MaskJson <1036934638@qq.com>
2019-01-14 18:01:10 +0800

    .

--- src/main/java/com/moving/admin/dao/common/AptnessDao.java
+++
@@ -1,8 +0,0 @@
-package com.moving.admin.dao.common;
-
-import com.moving.admin.entity.common.Aptness;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
-
-public interface AptnessDao extends JpaRepository<Aptness, Long>, JpaSpecificationExecutor<Aptness> {
-}
