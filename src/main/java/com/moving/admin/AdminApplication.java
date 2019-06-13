aae94451db637ef1544d6abf0f73f97d75d25abc
MaskJson <1036934638@qq.com>
2019-06-13 16:57:36 +0800

    .

--- src/main/java/com/moving/admin/AdminApplication.java
+++ src/main/java/com/moving/admin/AdminApplication.java
@@ -3,6 +3,8 @@ package com.moving.admin;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
@@ -10,7 +12,12 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @EntityScan(basePackages = "com.moving.admin.entity")
 @EnableScheduling
 @SpringBootApplication
-public class AdminApplication {
+public class AdminApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(AdminApplication.class);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(AdminApplication.class, args);
