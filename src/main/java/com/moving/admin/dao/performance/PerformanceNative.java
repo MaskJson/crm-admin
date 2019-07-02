3b0b548186cb1c6c0b432ae5255451d908db0953
MaskJson <1036934638@qq.com>
2019-07-02 12:00:24 +0800

    .

--- src/main/java/com/moving/admin/dao/performance/PerformanceNative.java
+++ src/main/java/com/moving/admin/dao/performance/PerformanceNative.java
@@ -66,10 +66,11 @@ public class PerformanceNative extends AbstractNative {
         } else {
             switch (Integer.parseInt(roleId.toString())) {
                 case 2:
-                case 6:
-                case 7: where = " where pr.create_user_id in(select user_id from team where "+levelFilter+" parent_id in(select id from team where level in(2,3,4) and user_id="+userId+"))";break;
-                case 3: where = " where pr.create_user_id in (select user_id from team where team_id in(select id from team where level=1 and user_id="+userId+"))";break;
-                case 1: where = " where pr.create_user_id in (select user_id from team where level=1)";break;
+//                case 6:
+                case 6: where = " where (pr.create_user_id="+userId+" or pr.create_user_id in(select user_id from team where "+levelFilter+" parent_id in(select id from team where level in(2,3,4) and user_id="+userId+")))";break;
+                case 3: where = " where (pr.create_user_id="+userId+" or pr.create_user_id in (select user_id from team where team_id in(select id from team where level=1 and user_id="+userId+")))";break;
+                case 1: where = " where 1=1";break;
+                default: where = " where cr.create_user_id="+userId;break;
             }
         }
         switch (flag) {
